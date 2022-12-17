bcee9abb7bc64b9c26e937ad2c571ea02b8c6c66
ion <utale.ion@gmail.com>
2022-12-17 11:20:17 +0000

    resize images on the fly working poc

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/discord/lilliput"
 )
 
 func main() {
@@ -11,13 +16,43 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+type ImageFormat struct {
+	Format string
+	Width  int
+	Height int
+	Quality int
+}
+
 func handleRequest(w http.ResponseWriter, r *http.Request) {
 	fileBytes, err := ioutil.ReadFile("test.png")
 	if err != nil {
 		panic(err)
 	}
 
-	newImage, err := processImage(fileBytes, "png", 100, 100)
+	// read query params into struct
+	format := r.URL.Query().Get("fm")
+	width := r.URL.Query().Get("w")
+	height := r.URL.Query().Get("h")
+	quality := r.URL.Query().Get("q")	
+
+	// convert query params to int
+	widthInt, err := strconv.Atoi(width)
+	if err != nil {
+		panic(err)
+	}
+
+	heightInt, err := strconv.Atoi(height)
+	if err != nil {
+		panic(err)
+	}
+
+	qualityInt, err := strconv.Atoi(quality)
+	if err != nil {
+		panic(err)
+	}
+
+	// process image
+	newImage, err := ProcessImage(fileBytes, format, widthInt, heightInt, qualityInt)
 	if err != nil {
 		panic(err)
 	}
@@ -25,5 +60,55 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/octet-stream")
 	w.Write(newImage)
-	return
+}
+
+func getEncodeOptions (format string, quality int) map[int]int {
+	if format == "jpeg" || format == "jpg" {
+		return map[int]int{lilliput.JpegQuality: quality}
+	} else if format == "png" {
+		return map[int]int{lilliput.PngCompression: quality}
+	} else if format == "webp" {
+		return map[int]int{lilliput.WebpQuality: quality}
+	} else {
+		return map[int]int{}
+	}
+}
+
+func ProcessImage(image []byte, format string, width int, height int, quality int) ([]byte, error) {
+	decoder, err := lilliput.NewDecoder(image)
+	// this error reflects very basic checks,
+	// mostly just for the magic bytes of the file to match known image formats
+	if err != nil {
+		fmt.Printf("error decoding image, %s\n", err)
+		os.Exit(1)
+	}
+	defer decoder.Close()
+
+	// get ready to resize image,
+	// using 8192x8192 maximum resize buffer size
+	ops := lilliput.NewImageOps(8192)
+	defer ops.Close()
+
+	fmt.Println(format, width, height, quality)
+
+	opts := &lilliput.ImageOptions{
+		FileType:             format,
+		Width:                width,
+		Height:               height,
+		ResizeMethod:         lilliput.ImageOpsFit,
+		NormalizeOrientation: true,
+		EncodeOptions:        getEncodeOptions(format, quality),
+	}
+
+	// create a buffer to store the output image, 50MB in this case
+	outputImg := make([]byte, 50*1024*1024)
+	
+	// resize and transcode image
+	outputImg, err = ops.Transform(decoder, opts, outputImg)
+	if err != nil {
+		fmt.Printf("error transforming image, %s\n", err)
+		panic(err)
+	}
+
+	return outputImg, err
 }
