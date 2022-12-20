bc8ecfea7578679149f7bb808870f9a1ea452d0a
ion <utale.ion@gmail.com>
2022-12-20 22:32:00 +0000

    content type twix

--- main.go
+++ main.go
@@ -61,6 +61,8 @@ func getImage_Handler(c *fiber.Ctx) error {
 	// w.WriteHeader(http.StatusOK)
 	// w.Header().Set("Content-Type", "application/octet-stream")
 	// w.Write(newImage)
-	c.Set("Content-Type", "application/octet-stream")
+	// c.Set("Content-Type", "application/octet-stream")
+	c.Set("Content-Type", "image/png")
+
 	return c.Send(newImage)
 }
